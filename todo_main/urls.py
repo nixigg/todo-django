2bfde68513463fce6c33986a754f015639dc5447
Sergiu <nixigg@gmail.com>
2023-08-16 13:16:48 -0700

    add task functionality

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -20,5 +20,8 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+
+    # To Do
+    path('todo/', include('todo.urls')),
 ]
